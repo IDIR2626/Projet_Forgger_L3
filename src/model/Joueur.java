39429307233249c321668bbfa20bee1b7c62a080
SidAli2025 <sidaliakhemoum.19@gmail.com>
2026-02-25 14:43:59 +0100

    mise à jour Joueur

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -1,13 +1,20 @@
 package model;
 
-public class Joueur {
+public class Joueur 
+{
 
     private int x;
     private int y;
+    private int vies;
+    private boolean bouclierActif;
+    private int dureeBouclier;
 
     public Joueur(int x, int y) {
         this.x = x;
         this.y = y;
+        this.vies = 3;            // valeur par défaut (modifiable selon ton jeu)
+        this.bouclierActif = false;
+        this.dureeBouclier = 0;
     }
 
     public int getX() {
@@ -18,16 +25,44 @@ public class Joueur {
         return y;
     }
 
+    public int getVies() {
+        return vies;
+    }
+
+    public boolean isBouclierActif() {
+        return bouclierActif;
+    }
+
+    public int getDureeBouclier() {
+        return dureeBouclier;
+    }
+
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     public void reinitialiser(int xDepart, int yDepart) {
-        setPosition(xDepart, yDepart);
+        this.x = xDepart;
+        this.y = yDepart;
+        this.vies = 3;
+        this.bouclierActif = false;
+        this.dureeBouclier = 0;
     }
 
-    public boolean deplacer(int deltaX, int deltaY, int largeurGrille, int hauteurGrille) {
+    public void activerBouclier(int duree) {
+        this.bouclierActif = true;
+        this.dureeBouclier = duree;
+    }
+
+    public void perdreVie() {
+        if (!bouclierActif) {
+            vies--;
+        }
+    }
+
+    public boolean deplacer(int deltaX, int deltaY, int largeurGrille, int hauteurGrille) 
+    {
         int nouvelleX = x + deltaX;
         int nouvelleY = y + deltaY;
 
@@ -40,6 +75,15 @@ public class Joueur {
 
         x = nouvelleX;
         y = nouvelleY;
+
+        // Gestion du bouclier (il se désactive quand la durée tombe à 0)
+        if (bouclierActif) {
+            dureeBouclier--;
+            if (dureeBouclier <= 0) {
+                bouclierActif = false;
+            }
+        }// Si le bouclier est actif, on le désactive après un déplacement (optionnel, selon les règles de ton jeu)
+
         return true;
     }
 }
